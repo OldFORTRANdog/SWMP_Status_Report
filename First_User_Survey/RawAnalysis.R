d2eb7c8f42f6414eae5cabbf5503e377cea8e465
Dave Eslinger <udave2007@gmail.com>
2018-11-10 19:41:33 -0500

    As used at 2018 NERRS/NERRA

--- First_User_Survey/RawAnalysis.R
+++ First_User_Survey/RawAnalysis.R
@@ -4,17 +4,48 @@
 require(tidyverse)
 require(readxl)
 
-dataDrive<-"D:"
-dataDir<-"SWMP_Stuff/Individual"
-df <- read_xlsx(paste(dataDrive,dataDir,"Excel/SWMP Status Report Initial Use.xlsx",sep = "/"))
+dataDrive<-"C:"
+#dataDir<-"SWMP_Stuff/Individual"
+dataDir<-"Users/davee/Documents/GitHub/SWMP_Status_Report/First_User_Survey"
+#df <- read_xlsx(paste(dataDrive,dataDir,"Excel/SWMP Status Report Initial Use.xlsx",sep = "/"))
+df <- read.csv(paste(dataDrive,dataDir,"CSV/SWMP Status Report Initial Use.csv",sep = "/"), stringsAsFactors = TRUE, sep =",")
 #head(df)
 df <- (df[-1,])
 df <- df[-c(2:9,12,14,16,18:20)]
 var_names <- c("ID","Sector","Used_Report","Updated_Report","Software_Rating","Process_Rating" )
-
 names(df) <- var_names
-for(i in 1:6) {
-  df[i] <- as.factor(df[i])
-  }
-levels(df$Sector)
-df
+lvl <- levels(df$Software_Rating)
+lvlOrdered <- lvl[c(5,2,4,3)]
+lvlOrdered[5] <- "It was too hard, I gave up."
+df$SoftOrered <- factor(df$Software_Rating, levels = lvlOrdered)
+lvl <- levels(df$Process_Rating)
+lvlOrdered[5] <- "It was too hard, I gave up."
+lvlOrdered <- lvl[c(5,2,3,4)]
+
+df$ProcessOrdered <- factor(df$Process_Rating, levels = lvlOrdered)
+#table(df$Sector)
+df$Sector <- recode(df$Sector, "Research Coordinators" = "RCs", 
+       "SWMP Technicians" = "SWMP Techs",
+       "Education Coordinators" = "ECs",
+       "Coastal Training Program Coordinators" = "CTPCs",
+       "Stewardship Coordinator" = "SCs")
+df %>%
+  ggplot(aes(factor(Sector))) %>%
+  + geom_bar(aes(fill = factor(Used_Report)))
+
+df %>%
+  #  group_by(Sector) %>%
+  ggplot(aes(factor(Sector))) %>%
+  + geom_bar(aes(fill = factor(Updated_Report)))
+
+df %>%
+  filter(Updated_Report == "Yes") %>%
+  ggplot(aes(factor(Sector))) %>%
+  + geom_bar(aes(fill = factor(ProcessOrdered)))
+
+df %>%
+  filter(Updated_Report == "Yes") %>%
+  ggplot(aes(factor(Sector))) %>%
+  + geom_bar(aes(fill = factor(SoftOrered)))
+             
+
